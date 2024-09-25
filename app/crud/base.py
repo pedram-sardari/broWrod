6bc4326d58b4e37072e200908efcccb30c7cb045
Pedram Sardari <pedisdi@gmail.com>
2024-09-25 22:33:01 +0330

    ✨ feat: Add `get_by_id` method to `UserCRUD` and `BaseCRUD`

--- app/crud/base.py
+++ app/crud/base.py
@@ -1,5 +1,6 @@
 from typing import TypeVar
 
+from bson import ObjectId
 from motor.motor_asyncio import AsyncIOMotorClient
 from pydantic import BaseModel
 
@@ -16,8 +17,16 @@ class BaseCRUD:
     def __init__(self, collection_name: str) -> None:
         self.col_name = collection_name
 
-    async def get(self, *, db_cli: AsyncIOMotorClient, query: dict) -> RetrieveSchemaType:
-        return await db_cli[self.db_name][self.col_name].find_one(query)
+    async def get(self, *, db_cli: AsyncIOMotorClient,
+                  query: dict) -> RetrieveSchemaType | None:  # todo: mismatch return
+        res_doc = await db_cli[self.db_name][self.col_name].find_one(query)
+        if res_doc:
+            res_doc["id"] = str(res_doc.pop("_id", None))
+        return res_doc
+
+    async def get_by_id(self, *, db_cli: AsyncIOMotorClient,
+                        id: str) -> RetrieveSchemaType | None:  # todo: mismatch return
+        return await self.get(db_cli=db_cli, query={"_id": ObjectId(id)})
 
     async def create(self, *, db_cli: AsyncIOMotorClient, obj_in: CreateSchemaType) -> CreateSchemaType:
         doc = obj_in.model_dump()
--- app/crud/user.py
+++ app/crud/user.py
@@ -3,14 +3,20 @@ import asyncio
 from motor.motor_asyncio import AsyncIOMotorClient
 
 from core.config import settings
-from crud.base import BaseCRUD, CreateSchemaType
 from core.security import get_password_hash
+from crud.base import BaseCRUD
 from schemas.user import UserInDB
 
 
 class UserCRUD(BaseCRUD):
     async def get_by_email(self, db_cli: AsyncIOMotorClient, email: str) -> UserInDB | None:
         user_doc = await super().get(db_cli=db_cli, query={"email": email})
+        if user_doc:
+            return UserInDB(**user_doc)
+        return None
+
+    async def get_by_id(self, db_cli: AsyncIOMotorClient, id: str) -> UserInDB | None:
+        user_doc = await super().get_by_id(db_cli=db_cli, id=id)
         if user_doc:
             return UserInDB(**user_doc)
         return None
@@ -33,7 +39,9 @@ user = UserCRUD(settings.USER_COLLECTION_NAME)
 if __name__ == "__main__":
     async def main():
         print('iner')
-        res = await user.get_by_email(db_cli=AsyncIOMotorClient(), email="john_doe@gmail.com")
+        # res = await user.get(db_cli=AsyncIOMotorClient(), query={"_id": ObjectId("66f42cef7b87f794b9d55621")})
+        # res = await user.get_by_email(db_cli=AsyncIOMotorClient(), email="john_doe@gmail.com")
+        res = await user.get_by_id(db_cli=AsyncIOMotorClient(), id="66f42cef7b87f794b9d55621")
         print(type(res))
         print(res)
 
