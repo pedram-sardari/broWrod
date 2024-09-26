470b005f5094edf64fa2cff040b0be21b5ecefbf
Pedram Sardari <pedisdi@gmail.com>
2024-09-26 12:42:38 +0330

    add create task

--- app/api/v1/endpoints/task.py
+++ app/api/v1/endpoints/task.py
@@ -1,3 +1,4 @@
+from bson import ObjectId
 from fastapi import APIRouter, Depends
 from motor.motor_asyncio import AsyncIOMotorClient
 
@@ -22,9 +23,10 @@ async def create_task(
 
 @router.get("/{task_id}", response_model=TaskRead)
 async def create_task(
+        task_id: str,
         current_user: UserRead = Depends(deps.get_current_user),
         db_cli: AsyncIOMotorClient = Depends(deps.get_db_cli)
 ):
-    task_in_db = await crud.task.get_by_id(db_cli=db_cli,
-                                        id=TaskInDB(user_id=current_user.id, **task_in.model_dump()))
-    return task_in_db
+    task_doc = await crud.task.get(db_cli=db_cli,
+                                   query={"_id": ObjectId(task_id), "user_id": current_user.id})
+    return task_doc
